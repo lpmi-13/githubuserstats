081e19f5eecc59e94f3ae70b7dcf5b62ca97ff06
lpmi-13 <leskis@gmail.com>
2017-08-27 09:55:43 +0100

    initial commit for basic user logins

---
+++ getUsers.py
@@ -0,0 +1,46 @@
+from github import Github
+import yaml
+import psycopg2
+
+with open('config.yaml', 'r') as GHyamlfile:
+    GHcreds = yaml.load(GHyamlfile)
+
+with open('db_config.yaml', 'r') as PGyamlfile:
+    PGcreds = yaml.load(PGyamlfile)
+
+#connect to postgres and get the highest user inserted
+conn = psycopg2.connect("dbname='github' user='{}' password='{}' host='localhost'".format(PGcreds['username'], PGcreds['password']))
+cursor = conn.cursor()
+
+cursor.execute('select UserId from users ORDER BY UserId DESC LIMIT 1;')
+
+user_number = cursor.fetchone()
+
+if (user_number is not None):
+    highest_user_id = user_number[0]
+else:
+    highest_user_id = 0
+
+g = Github(GHcreds['GITHUB_USERNAME'], GHcreds['GITHUB_PASSWORD'], timeout=200, per_page=30)
+
+user_list = g.get_users(since=highest_user_id)
+
+for user in user_list:
+    #get the specific user info
+    user = g.get_user(user.login)
+
+    #place user info in the DB for later
+    cursor.execute('INSERT INTO users (userid, login) VALUES (%s, %s)', (user.id, user.login))
+    conn.commit()
+    print('inserted entry for user {} into the DB'.format(user.id))
+    #get events per user (a merged PR is a type of event in Github)
+#    events = user.get_events()
+#    PR_ids = [event.id for event in events if event == 'PullRequestEvent']
+
+    #now loop through all PRs and get the corresponding repo info
+    
+    #for each repo, check all the issues, and filter by merged
+
+    #for all merged issues, if this user has one, increment count in the db
+
+conn.close()
