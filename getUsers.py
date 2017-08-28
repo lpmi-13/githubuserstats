7f4b608240ab6df934bf6f863edd4e4cfba74322
lpmi-13 <leskis@gmail.com>
2017-08-28 22:58:30 +0100

    add check for rate limit plus time logging

--- getUsers.py
+++ getUsers.py
@@ -2,6 +2,7 @@ from github import Github
 import yaml
 import psycopg2
 from rateLimit import return_rate_limit
+from datetime import datetime
 
 with open('config.yaml', 'r') as GHyamlfile:
     GHcreds = yaml.load(GHyamlfile)
@@ -22,28 +23,32 @@ if (user_number is not None):
 else:
     highest_user_id = 0
 
-g = Github(GHcreds['GITHUB_USERNAME'], GHcreds['GITHUB_PASSWORD'], timeout=200, per_page=30)
+g = Github(GHcreds['GITHUB_USERNAME'], GHcreds['GITHUB_PASSWORD'], timeout=500, per_page=30)
 
 user_list = g.get_users(since=highest_user_id)
 
 for user in user_list:
-    #get the specific user info
-    user = g.get_user(user.login)
 
-    #place user info in the DB for later
-    cursor.execute('INSERT INTO users (userid, login) VALUES (%s, %s)', (user.id, user.login))
-    conn.commit()
-    print('inserted entry for user {} into the DB'.format(user.id))
+    #get the specific user info
     rate = return_rate_limit(g)
     print('rate limit info: {}'.format(rate))
-    #get events per user (a merged PR is a type of event in Github)
-#    events = user.get_events()
-#    PR_ids = [event.id for event in events if event == 'PullRequestEvent']
+    print('{}'.format(str(datetime.now())))
 
-    #now loop through all PRs and get the corresponding repo info
+    if(rate > 4875):
+        user = g.get_user(user.login)
     
-    #for each repo, check all the issues, and filter by merged
-
-    #for all merged issues, if this user has one, increment count in the db
-
+        #place user info in the DB for later
+        cursor.execute('INSERT INTO users (userid, login) VALUES (%s, %s)', (user.id, user.login))
+        conn.commit()
+        print('inserted entry for user {} into the DB'.format(user.id))
+        #get events per user (a merged PR is a type of event in Github)
+    #    events = user.get_events()
+    #    PR_ids = [event.id for event in events if event == 'PullRequestEvent']
+    
+        #now loop through all PRs and get the corresponding repo info
+        
+        #for each repo, check all the issues, and filter by merged
+    
+        #for all merged issues, if this user has one, increment count in the db
+    
 conn.close()
