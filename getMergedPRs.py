ea17ebbb37db1054e7e5b96473ac8f0f1c31d0c5
lpmi-13 <leskis@gmail.com>
2018-06-17 12:16:39 +0100

    use actual correct search syntax

--- getMergedPRs.py
+++ getMergedPRs.py
@@ -17,26 +17,29 @@ g = Github(GHcreds['GITHUB_USERNAME'], GHcreds['GITHUB_PASSWORD'], timeout=200, 
 
 for login in just_logins:
 
-    issues = g.search_issues('author:{} is:pr state:merged'.format(login))
-    issue_repo_url_list = []
+    login_rate = return_rate_limit(g)
+    if (login_rate > 500):
+        issues = g.search_issues('author:{} is:pr is:merged'.format(login))
+        issue_repo_url_list = []
         
-    for issue in issues:
+        for issue in issues:
 
-        # so we don't get constant 403's from github
-        rate = return_rate_limit(g)
-        print('remaining API calls this hour: {}'.format(rate))
-        if(rate > 250):
-
-            # first, check if the PR was actually merged
-            PR_url = issue.pull_request.raw_data['url']
-            r = requests.get(PR_url)
-            json_data = json.loads(r.text)
-        
-            if json_data['merged'] == True:
-                issue_repo_url_list.append(issue.html_url)
-
-    # write to flat file for further analysis by getGraph.py  
-    with open('./data/{}-results'.format(login), 'w') as outputfile:
-        for url in issue_repo_url_list:
-            outputfile.write(url)
-            outputfile.write('\n')
+            print('grabbing {}'.format(issue.html_url))
+            # so we don't get constant 403's from github
+            rate = return_rate_limit(g)
+            print('remaining API calls this hour: {}'.format(rate))
+            if(rate > 250):
+    
+                # first, check if the PR was actually merged
+                PR_url = issue.pull_request.raw_data['url']
+                r = requests.get(PR_url)
+                json_data = json.loads(r.text)
+            
+                if json_data['merged'] == True:
+                    issue_repo_url_list.append(issue.html_url)
+    
+        # write to flat file for further analysis by getGraph.py  
+        with open('./data/{}-results'.format(login), 'w') as outputfile:
+            for url in issue_repo_url_list:
+                outputfile.write(url)
+                outputfile.write('\n')
